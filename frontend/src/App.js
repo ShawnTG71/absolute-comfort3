b7ccb48b2a1a912254710eaf2beaa00521eb458b
ShawnTG71 <shawntg-network-i.t@outlook.com>
2023-02-17 10:32:44 -0500

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Absolute Comfort</a>
+      </header>
+      <main>list products</main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
