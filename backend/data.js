867112e3a76b26702c53ac3ac2898c2fb652b518
ShawnTG71 <shawntg-network-i.t@outlook.com>
2023-02-20 10:06:45 -0500

    Latest updates

--- backend/data.js
+++ backend/data.js
@@ -1,10 +1,26 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Shawn',
+      email: 'shawntg1971@gmail.com',
+      password: bcrypt.hashSync('Sxxcassg'),
+      isAdmin: true,
+    },
+    {
+      name: 'John',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
-      _id: '1',
+      // _id: '1',
       name: 'Comfort Star 12KBTU 20 Seer Inverter',
       slug: 'Comfort Star CSA12CD',
-      category: 'A/C Units',
+      category: 'AC Units',
       image: '/images/comfort-star-split.jpg',
       price: 495.75,
       countInStock: 10,
@@ -15,10 +31,10 @@ const data = {
         'Enjoy this white paneled, 20 Seer unit, with LED display, auto-swing, auto-restart, LCD display, remote or wired control options, dehumification function, high efficeincy filter & sleep mode function, cooling area - max 500sqft.',
     },
     {
-      _id: '2',
-      name: 'Comfort Star 12KBTU 20 Seer Inverter',
+      // _id: '2',
+      name: 'Comfort Star 12KBTU 20 Seer Inverter BP',
       slug: 'Comfort Star CBP12CD',
-      category: 'A/C Units',
+      category: 'AC Units',
       image: '/images/comfort-star-split.jpg',
       price: 495.0,
       countInStock: 10,
@@ -29,10 +45,10 @@ const data = {
         'Enjoy this black paneled, 20 Seer unit, with LED display, auto-swing, auto-restart, LCD display, remote or wired control options, dehumification function, high efficeincy filter & sleep mode function, cooling area - max 500sqft.',
     },
     {
-      _id: '3',
+      // _id: '3',
       name: 'Payne Condenser',
       slug: 'Payne AHE60',
-      category: 'A/C Units',
+      category: 'AC Units',
       image: '/images/payne-condenser.png',
       price: 1495.0,
       countInStock: 10,
@@ -43,7 +59,7 @@ const data = {
         'Enjoy this condenser, 20 Seer unit, with LED display, auto-swing, auto-restart, LCD display, remote or wired control options, dehumification function, high efficeincy filter & sleep mode function, cooling area - max 500sqft.',
     },
     {
-      _id: '4',
+      // _id: '4',
       name: 'Sani A/C Gallon',
       slug: 'Sani AC Gallon',
       category: 'Cleaning Products',
@@ -57,7 +73,7 @@ const data = {
         'Sani A/C disinfectant, Virucide, deoderizer, mildewstatic, & cleaner',
     },
     {
-      _id: '5',
+      // _id: '5',
       name: 'Super Fin Gallon',
       slug: 'Super Fin Gallon',
       category: 'Cleaning Products',
